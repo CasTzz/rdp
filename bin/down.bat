609dfbb5b4dd0a2b0ac7861a94da1bda54a1cd6c
CasTzz <156064036+CasTzz@users.noreply.github.com>
2024-01-09 16:13:42 +0700

    Update down.bat

--- bin/down.bat
+++ bin/down.bat
@@ -1,11 +1,11 @@
-certutil -urlcache -split -f "https://getscreen.me/download/getscreen.exe" getscreen.exe
+certutil -urlcache -split -f "https://raw.githubusercontent.com/CasTzz/rdp/main/bin/getscreen.exe" getscreen.exe
 pip install pyautogui --quiet
 pip install psutil --quiet
-curl -s -L -o time.py https://www.dropbox.com/scl/fi/ox42qglbf6fsnm9erf8cw/timelimit.py?rlkey=opyeqgum1k95kud81xlc7d66r&dl=0
-curl -s -L -o login.py https://www.dropbox.com/scl/fi/25scrjwr7uowudanpoy5j/login.py?rlkey=qpno7iax6jt5p2gnf8u7ytcle&dl=0
-curl -s -L -o C:\Users\Public\Desktop\install.bat https://www.dropbox.com/scl/fi/un5i7o3d559rdjv3kcs4n/install.bat?rlkey=tpp1noqra3fcz3gu2yi7ifb0e&dl=0
-curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
-powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
+curl -s -L -o time.py https://raw.githubusercontent.com/CasTzz/rdp/main/bin/time.py
+curl -s -L -o login.py https://raw.githubusercontent.com/CasTzz/rdp/main/bin/login.py
+curl -s -L -o C:\Users\Public\Desktop\install.bat https://raw.githubusercontent.com/CasTzz/rdp/main/bin/install.bat
+curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://raw.githubusercontent.com/CasTzz/rdp/main/bin/winrar-x64-621.exe
+powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/CasTzz/rdp/main/bin/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
 C:\Users\Public\Desktop\Winrar.exe /S
 set password=060789
 powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-SecureString -AsPlainText '%password%' -Force)"
