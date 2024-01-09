3ddbcc2dd4fae9a767a854a88cc978e52f820f3e
CasTzz <156064036+CasTzz@users.noreply.github.com>
2024-01-09 16:15:02 +0700

    Update install.bat

--- bin/install.bat
+++ bin/install.bat
@@ -1,4 +1,4 @@
 curl -s -L -o dolphin.exe https://gh.dolphin-anty-cn.online/dolphinrucom/anty-releases/releases/download/v2022.326.2/dolphin-anty-win-latest.exe
-curl -s -L -o install.py https://www.dropbox.com/scl/fi/aaseffegvrmiwsi3gfr4k/install.py?rlkey=aaosclkdpm6k9q8hr9ioem14l&dl=0
+curl -s -L -o install.py https://raw.githubusercontent.com/CasTzz/rdp/main/bin/subbin_install_bat/install.py
 start dolphin.exe
 python install.py
